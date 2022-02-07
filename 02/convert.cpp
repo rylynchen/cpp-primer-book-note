baca9b15e581035fdba07c8ff8935638b38cd622
liuchen3 <liuchen3@lixiang.com>
2022-02-07 19:52:30 +0800

    up

---
+++ 02/convert.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+int stonetolb(int);
+
+int main()
+{
+    int stone;
+    cout << "Enter the weight in stone: ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " stone = ";
+    cout << pounds << " pound." << endl;
+    return 0;
+}
+
+int stonetolb(int sts)
+{
+    return 14 * sts;
+}
