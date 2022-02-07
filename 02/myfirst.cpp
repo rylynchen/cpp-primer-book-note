651039a7f52aa1314a61c4c7deb8d2237836aa59
liuchen3 <liuchen3@lixiang.com>
2022-02-07 19:31:12 +0800

    init

---
+++ 02/myfirst.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(void)                                  // function header
+{
+    // using namespace std;
+    using std::cout;
+    using std::endl;
+    using std::cin;
+    cout << "Com up and C++ me some time.";
+    cout << endl;
+    cout << "Your won't regret it!" << endl;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## C++ Primer Booke Note
+<details open>
+<summary>Note:</summary>
+
+- [x] [02-开始学习C++](02/README.md)
+</details>
+
+## Reference
+https://github.com/ShujiaHuang/Cpp-Primer-Plus-6th
